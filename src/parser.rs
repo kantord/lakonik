7bd7f2287ab5e69e4c98449d9fcd7c9410da0a0b
Daniel Kantor <git@daniel-kantor.com>
2025-05-18 23:25:46 +0200

    add some tests

--- src/parser.rs
+++ src/parser.rs
@@ -1,24 +1,24 @@
 use nom::Parser;
 use nom::character::complete::char;
 use nom::character::complete::{multispace1, one_of};
-use nom::combinator::{map, recognize};
+use nom::combinator::{all_consuming, map, recognize};
 use nom::multi::many1;
 use nom::{IResult, branch::alt};
 
 /// Names the entity you are talking to
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 struct Vocative {
     name: String,
 }
 
 /// Verbs are actions/capabilities the entity is expected to perform
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 struct Verb {
     name: String,
 }
 
 /// The fully-parsed sentence. Describes a prompt.
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct Sentence {
     vocative: Vocative,
     verb: Verb,
@@ -74,5 +74,45 @@ fn sentence(input: &str) -> IResult<&str, Sentence> {
 }
 
 pub fn parse_statement(input: &str) -> IResult<&str, Sentence> {
-    sentence.parse(input)
+    all_consuming(sentence).parse(input)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case("john run", "john", "run")]
+    #[case("alice42 jump", "alice42", "jump")]
+    #[case("my-name jump-fast", "my-name", "jump-fast")]
+    #[case("bob123 fly", "bob123", "fly")]
+    #[case("john-doe123 run-fast", "john-doe123", "run-fast")]
+    #[case("john- run", "john-", "run")]
+    fn test_parse_statement_success(
+        #[case] input: &str,
+        #[case] vocative: &str,
+        #[case] verb: &str,
+    ) {
+        let expected = Sentence {
+            vocative: Vocative {
+                name: vocative.to_string(),
+            },
+            verb: Verb {
+                name: verb.to_string(),
+            },
+        };
+
+        assert_eq!(parse_statement(input), Ok(("", expected)));
+    }
+
+    #[rstest]
+    #[case("42run")]
+    #[case("john run!")]
+    #[case(" run")]
+    #[case("")]
+    #[case("alice! jump")]
+    fn test_parse_statement_failure(#[case] input: &str) {
+        assert!(parse_statement(input).is_err());
+    }
 }
