524796ed28a37eea827d37f3b05974e335acab0a
Daniel Kantor <git@daniel-kantor.com>
2025-05-25 16:11:43 +0200

    include built-in templates

--- src/verbs.rs
+++ src/verbs.rs
@@ -1,9 +1,36 @@
+use include_dir::{Dir, include_dir};
 use minijinja::Environment;
+
+static BUILT_IN_TEMPLATES_DIR: Dir = include_dir!("$CARGO_MANIFEST_DIR/templates");
+
+pub struct Template {
+    pub path: String,
+    pub contents: String,
+}
+
+pub fn get_built_in_templates() -> impl Iterator<Item = Template> {
+    BUILT_IN_TEMPLATES_DIR
+        .find("**/*")
+        .expect("Failed to traverse embedded templates")
+        .filter_map(|entry| {
+            entry.as_file().map(|f| Template {
+                path: f.path().to_string_lossy().into_owned(),
+                contents: f
+                    .contents_utf8()
+                    .expect("Invalid UTF-8 in embedded template")
+                    .to_string(),
+            })
+        })
+}
 
 pub fn build_environment() -> Environment<'static> {
     let mut env = Environment::new();
 
-    let _ = env.add_template("create", "create for me a(n) {{description}}");
+    for template in get_built_in_templates() {
+        let name = template.path.replace("templates/", "");
+        env.add_template_owned(name, template.contents)
+            .expect("Failed to add template");
+    }
 
     env
 }
