272606fd6486290b2c4e59ee53af0dda1e60de7f
Daniel Kantor <git@daniel-kantor.com>
2025-06-10 22:59:23 +0200

    refactor: move verb related things into a separate hir module

--- src/hir/mod.rs
+++ src/hir/mod.rs
@@ -1,10 +1,11 @@
 #![allow(dead_code)]
-use lsp_types::Range;
 
-use crate::{
-    ast::{FilePathPart, FreeformPart, InlineShellPart, Part, Sentence, Verb, Vocative},
-    templates::{get_all_templates, get_user_templates},
-};
+pub mod verb;
+
+use lsp_types::Range;
+use verb::AnalyzedVerb;
+
+use crate::ast::{FilePathPart, FreeformPart, InlineShellPart, Part, Sentence, Vocative};
 
 pub struct AnalysisContext {}
 
@@ -49,13 +50,6 @@ pub enum AnalyzedPart {
     InlineShell(AnalyzedInlineShellPart),
 }
 
-#[derive(Debug, Clone)]
-pub struct AnalyzedVerb {
-    pub node: Verb,
-    pub template_name: String,
-    pub hover_text: String,
-}
-
 #[derive(Clone, Debug)]
 pub struct AnalyzedSentence {
     pub node: Sentence,
@@ -98,37 +92,6 @@ impl Analyzable for Part {
     }
 }
 
-impl Analyzable for Verb {
-    type AnalyzedNode = AnalyzedVerb;
-
-    fn analyze(&self, _ctx: &mut AnalysisContext) -> Self::AnalyzedNode {
-        let template_name = match self {
-            Verb::Simple(node) => node.name.clone(),
-            Verb::Assignment(node) => node.name.clone(),
-        };
-        let template_name = format!("verbs/{}", template_name);
-        let mut template_source = get_all_templates()
-            .find(|t| t.path == template_name)
-            .map(|t| t.contents.clone())
-            .unwrap_or_else(|| "*N/A*".to_string());
-
-        if let Verb::Assignment(node) = self {
-            template_source = node.value.clone();
-        }
-
-        let hover_text = format!(
-            "_Verb_ **{}**\n\n```\n{}\n```",
-            template_name, template_source
-        );
-
-        AnalyzedVerb {
-            node: self.clone(),
-            template_name,
-            hover_text,
-        }
-    }
-}
-
 impl Analyzable for Sentence {
     type AnalyzedNode = AnalyzedSentence;
 
@@ -143,34 +106,9 @@ impl Analyzable for Sentence {
     }
 }
 
-impl AnalyzedVerb {
-    /// Creates a template if it doeds not exist but can be created
-    pub fn ensure_template(&self) {
-        match &self.node {
-            Verb::Simple(_) => (),
-            Verb::Assignment(node) => {
-                let template_name = format!("verbs/{}", node.name);
-
-                if !get_user_templates().any(|t| t.path == template_name) {
-                    crate::templates::create_user_template(&template_name, &node.value);
-                }
-            }
-        }
-    }
-}
-
 impl Analyzed for AnalyzedVocative {
     fn get_range(&self) -> &Range {
         &self.node.range
-    }
-}
-
-impl Analyzed for AnalyzedVerb {
-    fn get_range(&self) -> &Range {
-        match &self.node {
-            Verb::Simple(node) => &node.range,
-            Verb::Assignment(node) => &node.range,
-        }
     }
 }
 
